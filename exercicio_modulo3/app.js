786f4031263c9654ba83f28c707755f852997ba0
carlosweb <kaduinfo@gmail.com>
2020-07-09 14:10:29 -0300

    Exercicios Realizados

--- exercicio_modulo3/app.js
+++ exercicio_modulo3/app.js
@@ -32,6 +32,9 @@ array "brazilianFoods" (null) seja substituído por "Pão de Queijo";
 - Exiba apenas o 2º item desse array no console.
 */
 
+brazilianFoods[1] = "Pão de Queijo"
+console.log(brazilianFoods[1])
+
 
 
 /*
@@ -47,6 +50,12 @@ mas sem digitar os nomes das comidas diretamente;
 */
 
 
+const foodsInfo = `"foodsInfo" a seguinte string: "Até aqui, o  
+array "brazilianFoods" possui ${brazilianFoods.length} itens: ${brazilianFoods[0]},  
+${brazilianFoods[1]} e ${brazilianFoods[2]}"`
+
+console.log(foodsInfo)
+
 
 /*
 06 - Comente o console.log acima e:
@@ -59,7 +68,10 @@ itens "Lilica" e "Matilda";
 - Exiba a "dogNames" no console.
 */
 
-
+const maleDogNames = ['Chico', 'Zeca']
+const femaleDogNames = ['Lilica', 'Matilda']
+let dogNames = maleDogNames.concat(femaleDogNames)
+console.log(dogNames.pop())
 
 /*
 07 - Comente o console.log acima e:
@@ -69,7 +81,7 @@ itens "Lilica" e "Matilda";
 - O resultado deve ser: "nome1, nome2, nome3, nome4".
 */
 
-
+console.log(dogNames.join(', '))
 
 /*
 08 - Comente o console.log acima e:
@@ -82,6 +94,8 @@ utilizando o método adequado para isso;
 - O console deve continuar exibindo o último item do array, que  
 agora é "Lilica".
 */
+
+console.log(dogNames.pop())
 
 
 
@@ -97,13 +111,18 @@ apenas as letras iniciais dos nomes do array "dogNames";
 */
 
 
+dogNames.push('Nina')
+
+const initials = `${dogNames[0].slice(0, 1)} ${dogNames[1].slice(0, 1)} ${dogNames[2].slice(0, 1)}`
+console.log(initials)
+
 
 /*
 10 - Comente o console.log acima e:
 - Exiba a "initials" no console, mas com todas as letras minúsculas;
 - Utilize o método adequado para isso.
 */
-
+console.log(initials.toLowerCase())
 
 
 /*
@@ -120,7 +139,8 @@ constante que você criou.
 'A SOBREMESA é um doce à base de coco, tradicional na América Latina e em Angola.'
 */
 
-
+const dessert = `${dogNames[0].slice(3,5)}${dogNames[1].slice(2,4)}${dogNames[2].slice(2,4).replace('n', 'd')}`
+console.log(`A ${dessert} é um doce à base de coco, tradicional na América Latina e em Angola.`)
 
 /*
 12 - Comente o console.log acima e:
@@ -133,3 +153,6 @@ elevado ao cubo, faça o 2º item do array receber todo o valor que ele
 já tem, mais 4;
 - Agora, o resultado exibido no console deve ser 729.
 */
+
+let oddNumbers = [3, 5, 7]
+console.log((oddNumbers[1] + 4) ** 3)
