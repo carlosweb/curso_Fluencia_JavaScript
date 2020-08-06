c8431f47c28330a7659abcc3e9f5cfcd69a6b6f9
Carlos Eduardo <kaduinfo@gmail.com>
2020-08-05 22:57:16 -0300

    Exercicio Pratico Popup

---
+++ ExercicioPraticoPopoup/app.js
@@ -0,0 +1,19 @@
+const bt = document.querySelector('button')
+const popup = document.querySelector('.popup-wrapper')
+
+
+bt.addEventListener('click', ()=> {
+    popup.style.display = "block"
+})
+
+popup.addEventListener('click', event => {
+    const classNameClickElement = event.target.classList[0]
+    const classNames = ['popup-close', 'popup-link', 'popup-wrapper']
+
+    const shouldClosepopoup = classNames.some(className => 
+        className === classNameClickElement)
+        
+    if(shouldClosepopoup){
+        popup.style.display = "none"
+    }
+})
