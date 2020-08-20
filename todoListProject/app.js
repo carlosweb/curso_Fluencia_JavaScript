cd9c762823ab196aac37314e0acfec03122b70a9
Carlos Eduardo <kaduinfo@gmail.com>
2020-08-20 18:39:51 -0300

    Exercicio 24 finalizado

--- todoListProject/app.js
+++ todoListProject/app.js
@@ -2,11 +2,11 @@ const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
 const inputSearchTodo = document.querySelector('.form-search input')
 
-
+// add todos
 const addTodoList = event => {
     event.preventDefault()
     const inputValue = event.target.add.value.trim()
-    if(inputValue.length){
+    if (inputValue.length) {
         todosContainer.innerHTML += `
         <li class="list-group-item d-flex justify-content-between align-items-center">
             <span>${inputValue}</span>
@@ -17,26 +17,29 @@ const addTodoList = event => {
     }
 }
 
+// search todos
 const searchTodoList = event => {
     const inputValue = event.target.value.trim().toLowerCase()
     Array.from(todosContainer.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
-    .forEach(todo => {
-        todo.classList.remove('d-flex')
-        todo.classList.add('hidden')
-    })
+        .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+        .forEach(todo => {
+            todo.classList.remove('d-flex')
+            todo.classList.add('hidden')
+        })
     Array.from(todosContainer.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
-    .forEach(todo => {
-        todo.classList.remove('hidden')
-        todo.classList.add('d-flex')
-    })
+        .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+        .forEach(todo => {
+            todo.classList.remove('hidden')
+            todo.classList.add('d-flex')
+        })
 }
 
+// delete todos
 const deleteTodosList = event => {
     const clickedElement = event.target
-    if(clickedElement.classList.contains('delete')) {
-        clickedElement.parentElement.remove()
+    const li = document.querySelector('.list-group-item')
+    if (clickedElement.classList.contains('delete')) {
+        li.remove()
     }
 }
 
