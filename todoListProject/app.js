cf3060de9e6fd7791265cf1d3b408e21f47a5149
Carlos Eduardo <kaduinfo@gmail.com>
2020-08-19 23:18:12 -0300

    Adicionado delete e busca no js

--- todoListProject/app.js
+++ todoListProject/app.js
@@ -1,12 +1,11 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
+const inputSearchTodo = document.querySelector('.form-search input')
 
 
-formAddTodo.addEventListener('submit', event => {
+const addTodoList = event => {
     event.preventDefault()
-
     const inputValue = event.target.add.value.trim()
-
     if(inputValue.length){
         todosContainer.innerHTML += `
         <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -16,4 +15,31 @@ formAddTodo.addEventListener('submit', event => {
         `
         event.target.reset()
     }
-})
+}
+
+const searchTodoList = event => {
+    const inputValue = event.target.value.trim().toLowerCase()
+    Array.from(todosContainer.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+        todo.classList.remove('d-flex')
+        todo.classList.add('hidden')
+    })
+    Array.from(todosContainer.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+        todo.classList.remove('hidden')
+        todo.classList.add('d-flex')
+    })
+}
+
+const deleteTodosList = event => {
+    const clickedElement = event.target
+    if(clickedElement.classList.contains('delete')) {
+        clickedElement.parentElement.remove()
+    }
+}
+
+todosContainer.addEventListener('click', deleteTodosList)
+formAddTodo.addEventListener('submit', addTodoList)
+inputSearchTodo.addEventListener('input', searchTodoList)
