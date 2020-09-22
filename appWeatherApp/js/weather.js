b5e009cf4da22f268029894badf7d6457eea98f8
Carlos Eduardo <kaduinfo@gmail.com>
2020-09-21 21:00:22 -0300

    App weather em desenvolvimento Exercicio 33 iniciado

--- appWeatherApp/js/weather.js
+++ appWeatherApp/js/weather.js
@@ -1,7 +1,7 @@
 const apiKey = 'I3oP4SGg2WsHsL83DLpIrWO72ViqEDLd'
 
 const getCityUrl = cityName => 
-`http://dataservice.accuweather.com/locations/v1/cities/search?apiKey=${apiKey}&q=${cityName}`
+`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
 
 const getCityData = async cityName => {
     try{
@@ -13,10 +13,28 @@ const getCityData = async cityName => {
         }
 
         const [cityData] = await response.json()
-        console.log(cityData)
+        return cityData
     } catch({ name, message }) {
         alert(`${name} : ${message}`)
     }
 }
 
-getCityData('São Paulo')
+const getCityWeather = async cityName => {
+    try{
+        const { Key } = await getCityData(cityName)
+        const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${apiKey}`
+        const response = await fetch(cityWeatherUrl)
+
+        if(!response.ok){
+            throw new Error('Não foi possivel obter os dados')
+        }
+
+        const [cityWeatherData] = await response.json()
+        
+        return cityWeatherData
+    } catch({ name, message }) {
+        alert(`${name} : ${message}`)
+    }
+}
+
+getCityWeather('Recife')
